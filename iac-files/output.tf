0a5f8ac60488af0e71c60bb02f8085bbff4c20f0
Ziya Asici <ziya.asici@yahoo.com>
2024-04-24 20:07:05 +0000

    test

--- iac-files/output.tf
+++ iac-files/output.tf
@@ -1,3 +1,3 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.ziyaasici-ECR.repository_url
-}
+# output "ecr_repository_url" {
+#   value = aws_ecr_repository.ziyaasici-ECR.repository_url
+# }
