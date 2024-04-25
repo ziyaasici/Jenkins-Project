20b07d1efb31dca85533d10946f58952741d8d3c
Ziya Asici <ziya.asici@yahoo.com>
2024-04-25 01:27:06 +0000

    Test keypair

--- terraform-files/test.tf
+++ terraform-files/test.tf
@@ -1,15 +1,15 @@
-resource "aws_key_pair" "key-pair" {
-    key_name = "docker-key-pair"
+resource "aws_key_pair" "TF_key" {
+    key_name = "TF_key"
     public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "tls_private_key" "rsa" {
     algorithm = "RSA"
-    rsa_bits = 2048
+    rsa_bits = 4096
 }
 
 resource "local_file" "docker-key" {
     content = tls_private_key.rsa.public_key_openssh
-    filename = "docker-key-pair.pem"
+    filename = "tfkey"
 }
 
