61d1bcb294bfd8bce7039a41710afd7a60ef724e
Ziya Asici <ziya.asici@yahoo.com>
2024-04-25 05:54:15 +0000

    Re-try keypair

--- terraform/terra-infra/test.tf
+++ terraform/terra-infra/test.tf
@@ -1,23 +1,25 @@
-# resource "aws_key_pair" "TF_key" {
-#     key_name = "TF_key"
-#     public_key = tls_private_key.rsa.public_key_openssh
-# }
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
 
-# resource "tls_private_key" "rsa" {
-#     algorithm = "RSA"
-#     rsa_bits = 4096
-# }
+resource "aws_key_pair" "key_pair" {
+  key_name   = "docker-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
 
-# resource "local_file" "docker-key" {
-#     content = tls_private_key.rsa.public_key_openssh
-#     filename = "TF_key.pem"
-# }
-
-# resource "aws_instance" "tf-ec2" {
-#     ami           = "ami-0d7a109bf30624c99"
-#     instance_type = "t2.micro"
-#     key_name = "TF_key"
-# }
+resource "local_file" "private_key" {
+  content = tls_private_key.rsa.private_key_pem
+  filename = "docker-key"
+}
 
 
-
+resource "aws_instance" "Test" {
+    ami = "ami-04e5276ebb8451442"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.key_pair.key_name
+    tags = {
+        Name= "Test-REST"
+    }
+    depends_on = [ aws_key_pair.key_pair ]
+}
