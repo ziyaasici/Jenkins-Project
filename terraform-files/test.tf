6b4639f9abd9a7fb0cd8cf3b13963cac36937e23
Ziya Asici <ziya.asici@yahoo.com>
2024-04-25 00:47:09 +0000

    Test keypair

--- terraform-files/test.tf
+++ terraform-files/test.tf
@@ -1,15 +1,9 @@
-resource "aws_key_pair" "docker-key" {
-  key_name   = "docker-key"
-  public_key = file("${path.module}/docker-key.pub")
+resource "aws_key_pair" "my_keypair" {
+  key_name   = "my-terraform-key"
+  public_key = file("${path.module}/my_key.pub")
 }
 
-
-resource "aws_instance" "docker-ec2" {
-  ami           = "ami-04e5276ebb8451442"  # Replace with a valid AMI ID
-  instance_type = "t2.micro"  # Adjust the instance type as needed
-  key_name      = aws_key_pair.docker-key.key_name
-
-  tags = {
-    Name = "Docker-Server"
-  }
+output "private_key" {
+  value     = tls_private_key.my_key.private_key_pem
+  sensitive = true
 }
